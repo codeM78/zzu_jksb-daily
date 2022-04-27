d16f406c15149ccdc9a3d04cdd2c23337269a084
codeM78 <52130543+codeM78@users.noreply.github.com>
2022-04-27 10:00:41 +0800

    Update jksb.py

--- jksb.py
+++ jksb.py
@@ -124,8 +124,8 @@ def sign_in(id, pwd):
     tree = etree.HTML(text)
     nodes = tree.xpath('//*[@id="bak_0"]/div[5]/span')
     # 如果今日填报过就退出填报，直接返回msg
-    if nodes[0].text == "今日您已经填报过了":
-        return nodes[0].text
+    #if nodes[0].text == "今日您已经填报过了":
+        #return nodes[0].text
     r.close()
     del (r)
 
