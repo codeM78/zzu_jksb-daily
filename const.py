6747e9388ddc940cec220f8e79504fa476c75028
Sun1ght <113571411+Sun1ght@users.noreply.github.com>
2022-09-15 12:45:38 +0800

    Add files via upload

--- const.py
+++ const.py
@@ -7,3 +7,5 @@ from enum import Enum, unique
 class Const(Enum):
     # 此处定义常量
     USER_AGENT = r"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36 Edg/104.0.1293.54"
+
+
