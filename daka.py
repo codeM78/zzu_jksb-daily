6d9f8ced33a0c9aafc38fd19feba74cee960dee0
codeM78 <52130543+codeM78@users.noreply.github.com>
2022-11-29 13:51:03 +0800

    Update daka.py

--- daka.py
+++ daka.py
@@ -38,30 +38,23 @@ def sign_in(id, pwd, name="Turing", check_today=1):
     # 去除 ssl警告
     urllib3.disable_warnings()
 
-    # 设置代理
-    proxy = [
-        {
-            'http': 'http://220.169.232.165:9999',
-            'https': 'http://220.169.232.165:9999',
-        },
-        {
-            'http': 'http://120.41.134.161:8888',
-            'https': 'http://120.41.134.161:8888',
-        },
-        {
-            'http': 'http://182.105.201.172:9999',
-            'https': 'http://182.105.201.172:9999',
-        },
-        {
-            'http': 'http://117.70.41.218:8080',
-            'https': 'http://117.70.41.218:8080',
-        },
-        {
-            'http': 'http://114.233.219.27:8080',
-            'https': 'http://114.233.219.27:8080',
-        },
-    ]
+    # # 获取登录页面验证码
+    # imgurl = const.Const.LOGIN_VCODE_URL.value
+    # # 获取当前文件当前绝对路径
+    # path = os.getcwd()
+    # # 图片存储路径
+    # dest = path + r"/num_vcode.png"
+    # utils.imgurl2pic(imgurl=imgurl, dest=dest)
+    #
+    # # 将图片验证码转换为文本列表
+    # # num_vcode = utils.baidu_OCR(dest, use_accurate=True)
+    # num_vcode = utils.pic2vcode(dest)
+    # logging.info(f"此次获取的图片值为：{num_vcode}")
+    # if num_vcode == "":
+    #     logging.info(name + ": " + id + ":验证码为空，打卡失败\n")
 
+    # 设置验证码为空
+    num_vcode = ""
     # login
     headers = {
         'User-Agent': const.Const.USER_AGENT.value,
@@ -71,6 +64,8 @@ def sign_in(id, pwd, name="Turing", check_today=1):
     form = {
         "uid": id,
         "upw": pwd,
+        # 登录页面验证码
+        "ver6": num_vcode,
         "smbtn": "进入健康状况上报平台",
         "hh28": "969"  # 按照当前浏览器窗口大小计算
     }
@@ -87,12 +82,13 @@ def sign_in(id, pwd, name="Turing", check_today=1):
             adapter = SSLAdapter('TLSv1')
             s.mount('https://', adapter)
             # post请求记得关闭证书错误忽略 verify=False
+            # 这里所有的原来都是requests.(post|get)
             r = s.post("https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login", headers=headers, data=form,
-                              proxies=random.choice(proxy), timeout=(200, 200), verify=False)  # response为账号密码对应的ptopid和sid信息,timeout=60(sec)
+                       timeout=(200, 200), verify=False)  # response为账号密码对应的ptopid和sid信息,timeout=60(sec)
             logging.info("成功运行post请求")
         except Exception as e:
             logging.error("请检查网络链接是否正常")
-            logging.error(e)
+            logging.error(str(e) + "\n")
             curr_punch += 1
             if curr_punch > max_punch:
                 exit()
@@ -110,9 +106,10 @@ def sign_in(id, pwd, name="Turing", check_today=1):
         ptopid = matchObj.group(1)
         sid = matchObj.group(2)
 
-    except:
-        logging.error("请检查账号" + id + "和密码" + pwd + "是否正确，或检查是否有验证码")
-        return "今日打卡失败！请手动打卡。（error：检查是否有验证码）。"
+    except Exception as e:
+        logging.error("请检查账号" + id + "和密码" + pwd + "是否正确，或检查是否有验证码\n")
+        logging.error("ptopid和sid获取失败：" + str(e))
+        return "今日打卡失败！请手动打卡。（error：请检查账号和密码是否正确，或是否有验证码）。"
         exit()
     else:
         logging.info("账号密码正确")
@@ -125,9 +122,9 @@ def sign_in(id, pwd, name="Turing", check_today=1):
         try:
             r = s.get(
                 "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb?ptopid=" + ptopid + "&sid=" + sid + "&fun2=",
-                proxies=random.choice(proxy), headers=headers, verify=False)  # response里含有jksb对应的params
+                headers=headers, verify=False)  # response里含有jksb对应的params
         except:
-            logging.error("get请求失败")
+            logging.error("get请求失败\n")
             if curr_punch > max_punch:
                 exit()
             curr_punch += 1
@@ -170,7 +167,7 @@ def sign_in(id, pwd, name="Turing", check_today=1):
         try:
             r = s.get(
                 "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb?ptopid=" + ptopid + "&sid=" + sid + "&fun2=",
-                proxies=random.choice(proxy), headers=headers, verify=False)  # response为jksb表单第一页
+                headers=headers, verify=False)  # response为jksb表单第一页
         except:
             logging.info("第二次get请求失败")
             while curr_punch > max_punch:
@@ -219,7 +216,7 @@ def sign_in(id, pwd, name="Turing", check_today=1):
     while True:
         try:
             r = s.post("https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb", headers=headers,
-                              proxies=random.choice(proxy), data=form, verify=False)  # response为打卡的第二个表单
+                       data=form, verify=False)  # response为打卡的第二个表单
         except:
             while curr_punch > max_punch:
                 exit()
@@ -231,33 +228,33 @@ def sign_in(id, pwd, name="Turing", check_today=1):
     r.close()
     del (r)
 
-    # 接下来获取 text中的图片值，并本地化保存
+    # # 接下来获取 text中的图片值，并本地化保存
     imgurl = utils.get_img_urls(html=text, index=0)
-    # 获取当前文件当前绝对路径
-    path = os.getcwd()
-    # 图片存储路径
-    dest = path + r"/vcode.png"
-    utils.imgurl2pic(imgurl=imgurl, dest=dest)
+    # 如果有验证码url
+    if imgurl != "找不到img标签":
+        # 获取当前文件当前绝对路径
+        path = os.getcwd()
+        # 图片存储路径
+        dest = path + r"/vcode.png"
+        utils.imgurl2pic(imgurl=imgurl, dest=dest)
 
-    # 将图片验证码转换为文本列表
-    vcode_list = utils.baidu_ORC(dest)
-    # 手写api为”“，高精度不为”“ 返回高精度
-    if vcode_list[0] == "" and vcode_list[1] != "":
-        vcode_text = vcode_list[1]
-    # 手写api为”“，高精度为”“ 返回常规
-    elif vcode_list[1] == "" and vcode_list[0] == "":
-        vcode_text = vcode_list[2]
-    # 其余情况返回 手写识别
+        # 将图片验证码转换为文本列表
+        vcode_text = utils.baidu_OCR(dest, use_hand=True)
+        if vcode_text == "":
+            logging.info(name + ": " + id + ":验证码为空，打卡失败\n")
+            return "很遗憾！今日打卡失败！请先自行打卡"
+        # 如果是大写中文数字再转换为阿拉伯数字
+        vcode = utils.chineseNumber2Num(vcode_text)
+        logging.info(f"此次获取的图片值为：{vcode_text}--->{vcode}")
+    # 如果没有验证码
     else:
-        vcode_text = vcode_list[0]
-    # 如果是大写中文数字再转换为阿拉伯数字
-    vcode = utils.chineseNumber2Num(vcode_text)
-    logging.info(f"此次获取的图片值为：{vcode_text}--->{vcode}")
+        vcode = ""
 
     # DONE  最后提交表单，跳转完成页面
     matchObj = re.search(r'name=\"ptopid\" value=\"(\w+)\".+name=\"sid\" value=\"(\w+)\"', text)
     ptopid = matchObj.group(1)
     sid = matchObj.group(2)
+
     form = {
         # 多了个验证码识别,需要将img图片下载下来，然后使用图片识别工具，识别出数字并赋值
         "myvs_94c": vcode,
@@ -289,13 +286,14 @@ def sign_in(id, pwd, name="Turing", check_today=1):
         "men6": "a",
         "sheng6": "",
         "shi6": "",
-        "fun218": fun218,  # fun218 动态变化，用来检测脚本
+        "fun218": fun218,  # 从fun18变成了fun218 动态变化，用来检测脚本
         "fun3": "",
         # "jingdu": "113.658333",  # 经度： 北校区经度,jingdu=113.658055
         # "weidu": "34.782222",  # 纬度： 北校区纬度,&weidu=34.782807  东经113.658333北纬34.7822222
         "ptopid": ptopid,
         "sid": sid,
     }
+
     headers = {
         'User-Agent': const.Const.USER_AGENT.value,
         'Referer': 'https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb',
@@ -315,7 +313,7 @@ def sign_in(id, pwd, name="Turing", check_today=1):
     while True:
         try:
             r = s.post("https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb", data=form,
-                              proxies=random.choice(proxy), headers=headers, verify=False)  # response为完成打卡页面
+                       headers=headers, verify=False)  # response为完成打卡页面
         except:
             while curr_punch > max_punch:
                 exit()
@@ -338,9 +336,9 @@ def sign_in(id, pwd, name="Turing", check_today=1):
         try:
             r = s.get(
                 "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb?ptopid=" + ptopid + "&sid=" + sid + "&fun2=",
-                proxies=random.choice(proxy), headers=headers, verify=False)  # response里含有jksb对应的params
+                headers=headers, verify=False)  # response里含有jksb对应的params
         except:
-            logging.error("get请求失败")
+            logging.error("get请求失败\n")
             if curr_punch > max_punch:
                 exit()
             curr_punch += 1
@@ -363,9 +361,8 @@ def sign_in(id, pwd, name="Turing", check_today=1):
         return name + ": 恭喜您，" + nodes[0].text
     else:
         logging.info(name + ": " + id + ":打卡失败\n")
-        return name + ": 很遗憾，今日打卡失败！请先自行打卡"
+        return "很遗憾！今日打卡失败！请先自行打卡"
 
     r.close()
     del (r)
-
 
